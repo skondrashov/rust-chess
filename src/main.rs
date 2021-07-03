8880607e0eeda07505aeb1e4e46b2545ce6cea5a
Sasha <sasha@thisminute.org>
2021-07-02 21:45:28 -0400

    change indexing so every 8th feature is a new piece

--- src/main.rs
+++ src/main.rs
@@ -10,79 +10,80 @@ const T: u8 = 1;
 const F: u8 = 0;
 
 fn main() {
+    // W, B, P, R, N, B, Q, K
     let board: [u8; INPUTS] = [
-        // White
-        T, T, T, T, T, T, T, T, // 1
-        T, T, T, T, T, T, T, T, // 2
+        // a
+        T, F, F, T, F, F, F, F, // 1
+        T, F, T, F, F, F, F, F, // 2
         F, F, F, F, F, F, F, F, // 3
         F, F, F, F, F, F, F, F, // 4
         F, F, F, F, F, F, F, F, // 5
         F, F, F, F, F, F, F, F, // 6
-        F, F, F, F, F, F, F, F, // 7
-        F, F, F, F, F, F, F, F, // 8
-        // Black
-        F, F, F, F, F, F, F, F, // 1
-        F, F, F, F, F, F, F, F, // 2
+        F, T, T, F, F, F, F, F, // 7
+        F, T, F, T, F, F, F, F, // 8
+        // b
+        T, F, F, F, T, F, F, F, // 1
+        T, F, T, F, F, F, F, F, // 2
         F, F, F, F, F, F, F, F, // 3
         F, F, F, F, F, F, F, F, // 4
         F, F, F, F, F, F, F, F, // 5
         F, F, F, F, F, F, F, F, // 6
-        T, T, T, T, T, T, T, T, // 7
-        T, T, T, T, T, T, T, T, // 8
-        // Pawn
-        F, F, F, F, F, F, F, F, // 1
-        T, T, T, T, T, T, T, T, // 2
+        F, T, T, F, F, F, F, F, // 7
+        F, T, F, F, T, F, F, F, // 8
+        // c
+        T, F, F, F, F, T, F, F, // 1
+        T, F, T, F, F, F, F, F, // 2
         F, F, F, F, F, F, F, F, // 3
         F, F, F, F, F, F, F, F, // 4
         F, F, F, F, F, F, F, F, // 5
         F, F, F, F, F, F, F, F, // 6
-        T, T, T, T, T, T, T, T, // 7
-        F, F, F, F, F, F, F, F, // 8
-        // Rook
-        T, F, F, F, F, F, F, T, // 1
-        F, F, F, F, F, F, F, F, // 2
+        F, T, T, F, F, F, F, F, // 7
+        F, T, F, F, F, T, F, F, // 8
+        // d
+        T, F, F, F, F, F, T, F, // 1
+        T, F, T, F, F, F, F, F, // 2
         F, F, F, F, F, F, F, F, // 3
         F, F, F, F, F, F, F, F, // 4
         F, F, F, F, F, F, F, F, // 5
         F, F, F, F, F, F, F, F, // 6
-        F, F, F, F, F, F, F, F, // 7
-        T, F, F, F, F, F, F, T, // 8
-        // Knight
-        F, T, F, F, F, F, T, F, // 1
-        F, F, F, F, F, F, F, F, // 2
-        F, F, F, F, F, F, F, F, // 3
-        F, F, F, F, F, F, F, F, // 4
-        F, F, F, F, F, F, F, F, // 5
-        F, F, F, F, F, F, F, F, // 6
-        F, F, F, F, F, F, F, F, // 7
+        F, T, T, F, F, F, F, F, // 7
         F, T, F, F, F, F, T, F, // 8
-        // Bishop
-        F, F, T, F, F, T, F, F, // 1
-        F, F, F, F, F, F, F, F, // 2
+        // e
+        T, F, F, F, F, F, F, T, // 1
+        T, F, T, F, F, F, F, F, // 2
         F, F, F, F, F, F, F, F, // 3
         F, F, F, F, F, F, F, F, // 4
         F, F, F, F, F, F, F, F, // 5
         F, F, F, F, F, F, F, F, // 6
-        F, F, F, F, F, F, F, F, // 7
-        F, F, T, F, F, T, F, F, // 8
-        // Queen
-        F, F, F, T, F, F, F, F, // 1
-        F, F, F, F, F, F, F, F, // 2
+        F, T, T, F, F, F, F, F, // 7
+        F, T, F, F, F, F, F, T, // 8
+        // f
+        T, F, F, F, F, T, F, F, // 1
+        T, F, T, F, F, F, F, F, // 2
         F, F, F, F, F, F, F, F, // 3
         F, F, F, F, F, F, F, F, // 4
         F, F, F, F, F, F, F, F, // 5
         F, F, F, F, F, F, F, F, // 6
-        F, F, F, F, F, F, F, F, // 7
-        F, F, F, T, F, F, F, F, // 8
-        // King
-        F, F, F, F, T, F, F, F, // 1
-        F, F, F, F, F, F, F, F, // 2
+        F, T, T, F, F, F, F, F, // 7
+        F, T, F, F, F, T, F, F, // 8
+        // g
+        T, F, F, F, T, F, F, F, // 1
+        T, F, T, F, F, F, F, F, // 2
         F, F, F, F, F, F, F, F, // 3
         F, F, F, F, F, F, F, F, // 4
         F, F, F, F, F, F, F, F, // 5
         F, F, F, F, F, F, F, F, // 6
-        F, F, F, F, F, F, F, F, // 7
-        F, F, F, F, T, F, F, F, // 8
+        F, T, T, F, F, F, F, F, // 7
+        F, T, F, F, T, F, F, F, // 8
+        // h
+        T, F, F, T, F, F, F, F, // 1
+        T, F, T, F, F, F, F, F, // 2
+        F, F, F, F, F, F, F, F, // 3
+        F, F, F, F, F, F, F, F, // 4
+        F, F, F, F, F, F, F, F, // 5
+        F, F, F, F, F, F, F, F, // 6
+        F, T, T, F, F, F, F, F, // 7
+        F, T, F, T, F, F, F, F, // 8
     ];
     let mut layer = Nnue {
         biases: [0f32; OUTPUTS],
